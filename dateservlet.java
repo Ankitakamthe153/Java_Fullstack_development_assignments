75c1bdafb36a1590b5635227c24467acc8a01459
Ankitakamthe153 <88307915+Ankitakamthe153@users.noreply.github.com>
2021-11-23 22:21:05 -0900

    Add files via upload

---
+++ dateservlet.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/welcome")
+public class dateservlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+	IOException{
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Welcome to the Servlet Programming.</h1>");
+		out.close();
+	}
+	
+
+}
